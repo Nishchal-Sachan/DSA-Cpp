716a3e69fe4cdd1d655ba9adc76d3bec30db021d
Nishchal-Sachan <sachannishchal@gmail.com>
2024-11-07 22:21:22 +0530

    Patterns Questions

---
+++ Patterns/CharactersRectangle.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // AAAAA
+    // BBBBB
+    // CCCCC
+    // DDDDD
+    // EEEEE
+    int row;
+    cout << "Enter no. of rows:";
+    cin >> row;
+    int i = 1;
+    char a = 'A';
+
+    while (i <= row)
+    {
+        int j = 1;
+        while (j <= row)
+        {
+            cout << a << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+        a++;
+    }
+    return 0;
+}

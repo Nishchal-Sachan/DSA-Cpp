d9df734b0d3a18c685dc7687345f9836b04c96ca
Nishchal-Sachan <sachannishchal@gmail.com>
2025-02-19 11:02:15 +0530

    Singly Linked List

---
+++ linkedList/singlyLinkedList.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public: 
+    int data;
+    Node* next;
+    
+    // Constructor
+    Node(int d){
+        this->data =d;
+        this->next=NULL;
+    }
+    
+    // Destructor
+    ~Node(){
+        cout<<"Node deleted successfully !"<<endl;
+    }
+};
+
+// insert at head
+void insertAtHead(Node* &head,int d){
+    Node* temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+// insert at tail
+void insertAtTail(Node* &tail,int d){
+    Node* temp = new Node(d);
+    tail->next=temp;
+    tail = tail->next;
+}
+
+// insert at any position
+void insert(Node* &head,int pos,int d){
+    if (pos==1){
+        insertAtHead(head,d);
+        return;
+    }
+    Node* temp = head;
+    int cnt=1;
+    while(cnt<pos-1){
+        temp=temp->next;
+        cnt++;
+    }
+    Node* toInsert= new Node(d);
+    toInsert->next = temp->next;
+    temp->next = toInsert;
+
+}
+
+// deleting nodes
+void deleteNode(Node* &head,int pos){
+    if(pos==1){
+        Node* temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+    }
+
+    else{
+        Node* curr = head;
+        Node*prev = NULL;
+        int cnt =1;
+        while(cnt<pos){
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+        prev->next =curr->next;
+        curr->next =NULL;
+        delete curr;
+    }
+}
+
+
+// traversing the linkedlist
+void print(Node* head){
+    Node* temp = head;
+    while (temp!=NULL){
+        cout<< temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    Node* node1= new Node(2);
+    cout<<node1->data<<endl;
+    cout<<node1->next<<endl;
+
+    Node* head = node1;
+    Node* tail = node1;
+    
+    insertAtHead(head,5);
+    print(head);
+    
+    insertAtTail(tail,9);
+    print(head);
+    
+    insert(head, 2,7);
+    print(head);
+
+    deleteNode(head ,2);
+    print(head);
+    return 0;
+}
+

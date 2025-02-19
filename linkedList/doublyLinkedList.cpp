7120d2d5b77d1612d01de40d50d6d993dc3d11b8
Nishchal-Sachan <sachannishchal@gmail.com>
2025-02-19 11:53:36 +0530

    Doubly Linked List

--- linkedList/doublyLinkedList.cpp
+++ linkedList/doublyLinkedList.cpp
@@ -1,16 +1,106 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node* prev;
-    Node* next;
+    Node *prev;
+    Node *next;
 
     // Constructor
-    Node(int d){
+    Node(int d)
+    {
         this->data = d;
-        this->prev =NULL;
-        this - >
+        this->prev = NULL;
+        this->next = NULL;
+    }
+
+    // Destructor
+    ~Node()
+    {
+        cout << " Node deleted successfully !" << endl;
     }
 };
+
+// insert at head
+void insertAtHead(Node *&head, int d)
+{
+    Node *temp = new Node(d);
+
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+}
+
+// insert at tail
+void insertAtTail(Node *tail, int d)
+{
+    Node *temp = new Node(d);
+
+    tail->next = temp;
+    temp->prev = tail;
+    tail = temp;
+}
+
+// deleting Node
+void deleteNode(Node *&head, int pos)
+{
+    if (pos == 1)
+    {
+        Node *temp = head;
+
+        temp->next->prev = NULL;
+        head = temp->next;
+        temp->next = NULL;
+        delete temp;
+    }
+    else
+    {
+        Node *curr = head;
+        Node *prev = NULL;
+
+        int cnt = 1;
+        while (cnt < pos)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+        prev->next = curr->next;
+        curr->next->prev = curr->prev;
+        curr->next = NULL;
+        curr->prev = NULL;
+        delete curr;
+    }
+}
+
+// traversing
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *node1 = new Node(10);
+
+    Node *head = node1;
+    Node *tail = node1;
+    print(head);
+
+    insertAtHead(head, 20);
+    print(head);
+
+    insertAtTail(tail, 25);
+    print(head);
+
+    deleteNode(head, 2);
+    print(head);
+}
